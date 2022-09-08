e32bd27cb6f0903748dc42c22162276aff66605b
unknown <laianhduy04042001@gmail.com>
2022-09-08 23:35:04 +0700

    Clean React Architecture.

---
+++ src/components/Task/Task.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Task.scss'
+
+function Task(){
+    return(
+        <li className="task-item">
+        <img src="https://i.imgur.com/i9TvwEL.jpg" alt="ảnh thanos" /> 
+          Title : Anh Duy
+        </li>
+    )
+}
+
+export default Task
